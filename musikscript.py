70758820d8b9e9e846a6f77bde444c8c49c0f675
lufey <lukas.fey@smail.th-koeln.de>
2023-11-23 12:28:49 +0100

    tried new extraction script(didnt work)

--- musikscript.py
+++ musikscript.py
@@ -6,8 +6,31 @@ if __name__ == '__main__':
     import librosa.display
     import matplotlib.pyplot as plt
     import IPython
-    #Replace 'input_audio_file' with your input audio file path
-    input_audio_file = 'guitardrums.wav'
+    from scipy.ndimage import median_filter
+
+
+def advanced_percussive_extraction(audio_path):
+    # Load audio file
+    y, sr = librosa.load(audio_path)
+    
+    # Apply Short-Time Fourier Transform (STFT)
+    stft = np.abs(librosa.stft(y))
+
+    # Perform median filtering along the frequency axis to emphasize percussive elements
+    # Adjust the filter size according to the characteristics of the audio
+    filtered_stft = median_filter(stft, size=(1, 25))  # Example filter size
+    
+    # Apply thresholding to discriminate between percussive and non-percussive components
+    threshold = 0.4 * np.max(filtered_stft)  # Adjust threshold according to the audio
+    percussive_mask = filtered_stft > threshold
+
+    # Apply the mask to the original spectrogram
+    percussive_stft = stft * percussive_mask
+
+    # Inverse STFT to obtain the percussive component
+    y_percussive = librosa.istft(percussive_stft)
+
+    return y_percussive
 
     # Load the original audio file
     y_original, sr = librosa.load(input_audio_file)
@@ -29,3 +52,7 @@ if __name__ == '__main__':
     # Save the separated components as WAV files
     sf.write('harmonic.wav', y_harmonic_time, sr)
     sf.write('percussive.wav', y_percussive_time, sr)
+    output_percussive_path ='advperc.wav'
+    advanced_percussive_extraction_and_save(input_audio_path, output_percussive_path)
+   
+
